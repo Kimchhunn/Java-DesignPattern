a74e013ce9ffda14e63760dde3f5bb9be40ead5b
Chhunnnn <30552313+Chhunnnn@users.noreply.github.com>
2018-03-22 15:24:09 +0700

    22.Mar.2018
    Sync Keyword and Sync Code BLock

--- SingletonDesignPattern/src/com/company/Main.java
+++ SingletonDesignPattern/src/com/company/Main.java
@@ -2,7 +2,54 @@ package com.company;
 
 public class Main {
 
+    public void learnNormalSingleton() {
+        for (int i =1; i < 5; i ++) {
+
+            Normal obj = Normal.getInstance();
+            if (obj == null) {
+                System.out.println("Object is already created.");
+            }
+            else {
+                System.out.println("Object is created.");
+            }
+        }
+    }
+
+    public void learnSingletonThreadSafeWithSyncKeyword() {
+        Thread a = new Thread(() -> {
+            SyncKeyword obj1 = SyncKeyword.getInstance();
+            System.out.println(obj1);
+        });
+        Thread b = new Thread(() -> {
+            SyncKeyword obj2 = SyncKeyword.getInstance();
+            System.out.println(obj2);
+        });
+        a.start();
+        b.start();
+    }
+
+    public void learnSingletonThreadSafeWithSyncCodeBlock () {
+        Thread a = new Thread(() -> {
+            SyncCodeBlock obj1 = SyncCodeBlock.getInstance();
+            System.out.println(obj1);
+        });
+        Thread b = new Thread(() -> {
+            SyncCodeBlock obj2 = SyncCodeBlock.getInstance();
+            System.out.println(obj2);
+        });
+        a.start();
+        b.start();
+    }
+
+    public void learnSingletonThreadSafeWitheEnum () {
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+//        main.learnNormalSingleton();
+//        main.learnSingletonThreadSafeWithSyncKeyword();
+        main.learnSingletonThreadSafeWithSyncCodeBlock();
+//        main.learnSingletonThreadSafeWitheEnum();
     }
 }
